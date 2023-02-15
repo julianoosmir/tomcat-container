bb5e553bcc51a83b16aaf6206ae5ee78241e78ac
juliano <julianoosmir@outlook.com>
2023-02-14 22:43:10 -0400

    jsp in docker

--- Cli.java
+++ Cli.java
@@ -2,15 +2,16 @@ public class Cli {
     public static void main(String[] args) {
         Runtime comando = Runtime.getRuntime();
         String build = "docker build . -t " + args[0] + ":latest";
-        String dockerun = "docker run --rm -d -p 8080:8080/tcp " + args[0] + ":latest";
+        //String dockerun = "docker run --rm -d -p 8080:8080/tcp " + args[0] + ":latest";
+        Strig compose = "docker-compose up";
         try {
 
             Process buildar = comando.exec(build);
             buildar.waitFor();
             System.out.println(build);
-            Process rodar = comando.exec(dockerun);
+            Process rodar = comando.exec(compose);
             rodar.waitFor();
-            System.out.println(dockerun);
+            System.out.println(compose);
 
 
         } catch (Exception e) {
