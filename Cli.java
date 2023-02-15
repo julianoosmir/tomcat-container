0fa9f502a5aa671a1ffdf7eb2294b6c844d8e571
juliano <julianoosmir@outlook.com>
2023-02-14 22:17:46 -0400

    novo cli

---
+++ Cli.java
@@ -0,0 +1,20 @@
+public class Cli {
+    public static void main(String[] args) {
+        Runtime comando = Runtime.getRuntime();
+        String build = "docker build . -t " + args[0] + ":latest";
+        String dockerun = "docker run --rm -d -p 8080:8080/tcp " + args[0] + ":latest";
+        try {
+
+            Process buildar = comando.exec(build);
+            buildar.waitFor();
+            System.out.println(build);
+            Process rodar = comando.exec(dockerun);
+            rodar.waitFor();
+            System.out.println(dockerun);
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
